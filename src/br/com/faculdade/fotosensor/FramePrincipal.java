cf56c317c2a2a608ab3b6b81e9038bf631e29994
Welington Sousa <welingtonwork@gmail.com>
2016-06-08 11:38:24 -0300

    Criando o botão para capturar a imagem

--- src/br/com/faculdade/fotosensor/FramePrincipal.java
+++ src/br/com/faculdade/fotosensor/FramePrincipal.java
@@ -1,10 +1,18 @@
 package br.com.faculdade.fotosensor;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.ParseException;
 
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import org.bridj.ann.Alignment;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
@@ -52,6 +60,36 @@ public class FramePrincipal extends JFrame {
 		painelPrincipal = new JPanel();
 		painelPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(painelPrincipal);
+
+		JButton botaoCapturar = new JButton("Capturar");
+		botaoCapturar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// trata o evento
+				try {
+					capturaImagem();
+					gravaTextoImagem();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+
+		// agrupa nosso layout
+		GroupLayout contentLayout = new GroupLayout(painelPrincipal);
+		contentLayout
+				.setHorizontalGroup(contentLayout.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
+						contentLayout.createSequentialGroup()
+								.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE).addComponent(botaoCapturar,
+										GroupLayout.PREFERRED_SIZE, 135, GroupLayout.PREFERRED_SIZE)
+								.addContainerGap()));
+		contentLayout.setVerticalGroup(
+				contentLayout.createParallelGroup(Alignment.LEADING).addGroup(contentLayout.createSequentialGroup()
+						.addComponent(botaoCapturar).addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		painelPrincipal.setLayout(contentLayout);
 
 		iniciaWebCam();
 	}
